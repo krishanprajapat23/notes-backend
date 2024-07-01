2b768b102c450468c911b9a7bf68aae4857d33fb
Krish <111554369+krishanprajapat23@users.noreply.github.com>
2024-07-01 13:33:49 +0530

    Token authentication
    Limiting creating new notes to logged-in users
    Problems of Token-based authentication
    End notes

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,19 @@
 const notesRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+
 const Note = require('../models/note')
 const User = require('../models/user')
+
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
+
 
 // notesRouter.get('/', (request, response) => {
 //   Note.find({}).then(notes => {
@@ -86,9 +99,15 @@ notesRouter.get('/:id', async (request, response) => {
 
 // with express-async-errors lib==============================================================================
 notesRouter.post('/', async (request, response) => {
-  const body = request.body
+  const body = request.body;
 
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  // const user = await User.findById(body.userId) //== without login
+  const user = await User.findById(decodedToken.id)
+
 
   const note = new Note({
     content: body.content,
